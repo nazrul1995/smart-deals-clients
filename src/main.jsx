f2112685375fcf4e99b63721fd5509e93ef2ba7d
Programming Hero <programming.hero1@gmail.com>
2025-10-30 09:45:06 -0500

    display latest products

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import AuthProvider from './contexts/AuthProvider.jsx';
 import Register from './components/Register/Register.jsx';
 import MyProducts from './components/MyProducts/MyProducts.jsx';
 import MyBids from './components/MyBids/MyBids.jsx';
+import ProductDetails from './components/ProductDetails/ProductDetails.jsx';
 
 const router = createBrowserRouter([
   {
@@ -37,6 +38,11 @@ const router = createBrowserRouter([
       {
         path: 'myBids',
         element: <MyBids></MyBids>
+      },
+      {
+        path: 'productDetails/:id',
+        loader: ({params}) => fetch(`http://localhost:3000/products/${params.id}`),
+        Component: ProductDetails
       }
     ]
   },
