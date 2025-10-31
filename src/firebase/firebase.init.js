c1e763212648b3fbb3da6ca9b897b8e0f2bce2ca
Programming Hero <programming.hero1@gmail.com>
2025-10-30 21:35:00 -0500

    added environment variable

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRzbErh_TkSoaSQg-xrDqutqvcm0tpswo",
-  authDomain: "smart-deals-6768c.firebaseapp.com",
-  projectId: "smart-deals-6768c",
-  storageBucket: "smart-deals-6768c.firebasestorage.app",
-  messagingSenderId: "954044857203",
-  appId: "1:954044857203:web:15cda9eff195bb88e0a306"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
